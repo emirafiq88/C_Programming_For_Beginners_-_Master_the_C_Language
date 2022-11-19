1efe91c0c70ec70977660ff1bd052c997dd1a59b
emirafiq88 <118448623+emirafiq88@users.noreply.github.com>
2022-11-19 18:04:13 +0800

    Forth Commit

---
+++ Challenge16/obj/main.c
@@ -0,0 +1,23 @@
+/********************************************************************/
+/* This program is about showing name at output                     */
+/* Author by : Emir Afiq Ridzuan                                    */
+/********************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+void square(int * x);
+
+int main()
+{
+   int num = 5;
+
+   square(&num);
+   printf("The square of the given number is %d\n", num);
+
+   return 0;
+}
+
+void square(int * x)
+{
+    *x = (*x) * (*x);
+}
